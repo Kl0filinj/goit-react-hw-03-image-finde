8d1f0127a45d554ea22aa16a6c64bc525ed93947
Velychko Daniil <101584452+Kl0filinj@users.noreply.github.com>
2022-10-05 16:00:00 +0200

    Refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,88 @@
 import React, { Component } from 'react';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
+import Loader from './Loader';
+import LoadMoreBtn from './LoadMoreBtn';
+import getImages from './services/api';
 import '../styles.css';
 
 export class App extends Component {
   state = {
     serchReqest: '',
+    status: 'idle',
+    imagesList: [],
+    page: 1,
+    modal: false,
+    error: null,
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.serchReqest !== this.state.serchReqest) {
+      this.setState({ status: 'pending', page: 1, imagesList: [] });
+      getImages(this.state.serchReqest, this.state.page)
+        .then(images => {
+          console.log('From Initial fetch', prevState.imagesList);
+          return this.setState({
+            imagesList: images.hits,
+            status: 'completed',
+          });
+        })
+        .catch(error => {
+          console.log('у вас Ошибка => ', error);
+          this.setState({ error, status: 'rejected' });
+        });
+    }
+    if (prevState.page !== this.state.page && this.state.page !== 1) {
+      this.setState({ status: 'pending' });
+
+      getImages(this.state.serchReqest, this.state.page)
+        .then(images => {
+          console.log(prevState.imagesList);
+          console.log(images.hits);
+          return this.setState(prState => ({
+            imagesList: [...prState.imagesList, ...images.hits],
+            status: 'completed',
+          }));
+        })
+        .catch(error => {
+          console.log('у вас Ошибка => ', error);
+          this.setState({ error, status: 'rejected' });
+        });
+    }
+  }
+
   onSubmit = req => {
     this.setState({ serchReqest: req });
   };
 
+  loadMoreBtnHandler = () => {
+    this.setState(prState => ({ page: prState.page + 1 }));
+  };
+
+  // toggleModalHandler = () => {
+  //   this.setState({ modal: !this.state.modal });
+  // };
+
   render() {
+    const { status, imagesList, error } = this.state;
     return (
       <div className="app">
         <Searchbar onSubmit={this.onSubmit} />
-        <ImageGallery serchReqest={this.state.serchReqest} />
+        {status === 'idle' && (
+          <h1 className="temporaty-heading">Enter your request ⬆️</h1>
+        )}
+        {status === 'error' && <h1 className="temporaty-heading">{error}</h1>}
+
+        <ImageGallery
+          serchReqest={this.state.serchReqest}
+          images={imagesList}
+        />
+
+        {status === 'pending' && <Loader />}
+
+        {imagesList.length > 0 && status === 'completed' && (
+          <LoadMoreBtn onClickHandler={this.loadMoreBtnHandler} />
+        )}
       </div>
     );
   }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,83 +1,74 @@
 import React, { Component } from 'react';
-import getImages from '../services/api';
 import ImageGalleryItem from '../ImageGalleryItem';
-import LoadMoreBtn from '../LoadMoreBtn';
-import { MagnifyingGlass } from 'react-loader-spinner';
+
 import './imageGallery.css';
 
 export default class ImageGallery extends Component {
-  state = {
-    status: 'idle',
-    imagesList: [],
-    page: 1,
-    modal: false,
-    error: null,
-  };
+  // state = {
+  //   status: 'idle',
+  //   imagesList: [],
+  //   page: 1,
+  //   modal: false,
+  //   error: null,
+  // };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.serchReqest !== this.props.serchReqest) {
-      this.setState({ status: 'pending', page: 1, imagesList: [] });
-      getImages(this.props.serchReqest, this.state.page)
-        .then(images => {
-          console.log('From Initial fetch', prevState.imagesList);
-          return this.setState({
-            imagesList: images.hits,
-            status: 'completed',
-          });
-        })
-        .catch(error => {
-          console.log('у вас Ошибка => ', error);
-          this.setState({ error, status: 'rejected' });
-        });
-    }
-    if (prevState.page !== this.state.page && this.state.page !== 1) {
-      this.setState({ status: 'pending' });
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevProps.serchReqest !== this.props.serchReqest) {
+  //     this.setState({ status: 'pending', page: 1, imagesList: [] });
+  //     getImages(this.props.serchReqest, this.state.page)
+  //       .then(images => {
+  //         console.log('From Initial fetch', prevState.imagesList);
+  //         return this.setState({
+  //           imagesList: images.hits,
+  //           status: 'completed',
+  //         });
+  //       })
+  //       .catch(error => {
+  //         console.log('у вас Ошибка => ', error);
+  //         this.setState({ error, status: 'rejected' });
+  //       });
+  //   }
+  //   if (prevState.page !== this.state.page && this.state.page !== 1) {
+  //     this.setState({ status: 'pending' });
 
-      getImages(this.props.serchReqest, this.state.page)
-        .then(images => {
-          console.log(prevState.imagesList);
-          console.log(images.hits);
-          return this.setState(prState => ({
-            imagesList: [...prState.imagesList, ...images.hits],
-            status: 'completed',
-          }));
-        })
-        .catch(error => {
-          console.log('у вас Ошибка => ', error);
-          this.setState({ error, status: 'rejected' });
-        });
-    }
-  }
+  //     getImages(this.props.serchReqest, this.state.page)
+  //       .then(images => {
+  //         console.log(prevState.imagesList);
+  //         console.log(images.hits);
+  //         return this.setState(prState => ({
+  //           imagesList: [...prState.imagesList, ...images.hits],
+  //           status: 'completed',
+  //         }));
+  //       })
+  //       .catch(error => {
+  //         console.log('у вас Ошибка => ', error);
+  //         this.setState({ error, status: 'rejected' });
+  //       });
+  //   }
+  // }
 
-  loadMoreBtnHandler = () => {
-    this.setState(prState => ({ page: prState.page + 1 }));
-  };
+  // loadMoreBtnHandler = () => {
+  //   this.setState(prState => ({ page: prState.page + 1 }));
+  // };
 
-  toggleModalHandler = () => {
-    this.setState({ modal: !this.state.modal });
-  };
+  // toggleModalHandler = () => {
+  //   this.setState({ modal: !this.state.modal });
+  // };
   render() {
-    const { status, imagesList, error } = this.state;
+    const { images } = this.props;
     return (
       <>
-        {status === 'idle' && (
-          <h1 className="temporaty-heading">Enter your request ⬆️</h1>
-        )}
-
-        {status === 'error' && <h1 className="temporaty-heading">{error}</h1>}
-        {/* {status === 'completed' && ( */}
-
         <ul className="imageGallery">
-          {imagesList.map(({ webformatURL, id }) => (
+          {images.map(({ webformatURL, id }) => (
             <ImageGalleryItem
               src={webformatURL}
               key={id}
-              toggleModalHandler={this.toggleModalHandler}
+              // toggleModalHandler={this.toggleModalHandler}
             ></ImageGalleryItem>
           ))}
         </ul>
 
-        {status === 'pending' && (
+        {/* {status === 'pending' && (
           <div className="loaderPlaceholder">
             <MagnifyingGlass
               visible={true}
@@ -93,7 +84,7 @@ export default class ImageGallery extends Component {
         )}
         {imagesList.length > 0 && status === 'completed' && (
           <LoadMoreBtn onClickHandler={this.loadMoreBtnHandler} />
-        )}
+        )} */}
 
         {/* )} */}
       </>
